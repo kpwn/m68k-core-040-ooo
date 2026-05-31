b80c579ff547880d9451dea28c70e56a120a3acf
qwertyoruiop <i_are@qwertyoruiop.com>
2026-05-31 09:40:24 +0200

    feat: ISA constants + Size/MemOp/Cluster enums

---
+++ src/test/scala/m68k040/isa/IsaSpec.scala
@@ -0,0 +1,16 @@
+package m68k040.isa
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class IsaSpec extends AnyFunSuite {
+  test("16 architectural integer registers (D0-D7, A0-A7)") {
+    assert(Isa.ARCH_INT_REGS == 16)
+  }
+  test("CCR bit positions match m68k (X N Z V C)") {
+    assert(Isa.CCR_C == 0 && Isa.CCR_V == 1 && Isa.CCR_Z == 2 && Isa.CCR_N == 3 && Isa.CCR_X == 4)
+  }
+  test("NZVC mask covers bits 3..0 and X mask is bit 4") {
+    assert(Isa.MASK_NZVC == 0xF)
+    assert(Isa.MASK_X == 0x10)
+  }
+}
